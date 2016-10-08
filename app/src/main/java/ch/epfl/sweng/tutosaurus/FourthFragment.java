f8a955164eff851d966750aebfa1c9257e505405
Vincent Rinaldi <Vincent Rinaldi>
2016-10-08 13:03:12 +0200

    Starting Point Of The App With Fragments And Burger Menu

---
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/FourthFragment.java
@@ -0,0 +1,25 @@
+package ch.epfl.sweng.tutosaurus;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vincent on 05/10/2016.
+ */
+
+public class FourthFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.fourth_layout, container, false);
+        ((HomeScreenActivity) getActivity()).setActionBarTitle("Settings");
+        return myView;
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/MainActivity.java
@@ -0,0 +1,22 @@
+package ch.epfl.sweng.tutosaurus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent myIntent = getIntent();
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, HomeScreenActivity.class);
+        startActivity(intent);
+    }
+}
