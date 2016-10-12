55ca225eca6b190f674b199c490b0066f8254291
Santo Gioia <santo.gioia@epfl.ch>
2016-10-12 15:35:08 +0200

    settings preference fragment has been implemented

--- app/src/main/java/ch/epfl/sweng/tutosaurus/FourthFragment.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/FourthFragment.java
@@ -2,24 +2,31 @@ package ch.epfl.sweng.tutosaurus;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 /**
  * Created by Vincent on 05/10/2016.
  */
 
-public class FourthFragment extends Fragment {
+public class FourthFragment extends PreferenceFragment {
 
     View myView;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView = inflater.inflate(R.layout.fourth_layout, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         ((HomeScreenActivity) getActivity()).setActionBarTitle("Settings");
-        return myView;
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.settings_preferences);
     }
+
+
+
 }
