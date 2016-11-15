31c7dd656884c15597b030b9ae71180800f32b2b
Santo Gioia <santo.gioia@epfl.ch>
2016-11-15 21:37:15 +0100

    commit pre merging

--- app/src/main/java/ch/epfl/sweng/tutosaurus/CreateMeetingActivity.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/CreateMeetingActivity.java
@@ -1,16 +1,11 @@
 package ch.epfl.sweng.tutosaurus;
 
-import android.app.DialogFragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
--- app/src/main/java/ch/epfl/sweng/tutosaurus/PublicProfileActivity.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/PublicProfileActivity.java
@@ -36,15 +36,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Calendar;
-
-import ch.epfl.sweng.tutosaurus.findTutor.FirebaseTutorAdapter;
 import ch.epfl.sweng.tutosaurus.helper.DatabaseHelper;
-import ch.epfl.sweng.tutosaurus.model.Meeting;
 import ch.epfl.sweng.tutosaurus.model.User;
+
 
 public class PublicProfileActivity extends AppCompatActivity {
 
