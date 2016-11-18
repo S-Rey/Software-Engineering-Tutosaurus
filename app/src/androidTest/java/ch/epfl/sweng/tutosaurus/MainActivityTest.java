8143e77a84babd0f93f057a7946525a94131f749
Vincent Rinaldi <Vincent Rinaldi>
2016-11-18 02:43:02 +0100

    Be A Tutor implemented and Tests for Simulating a Click on Preference Screen Added

--- app/src/androidTest/java/ch/epfl/sweng/tutosaurus/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/tutosaurus/MainActivityTest.java
@@ -1,27 +1,29 @@
 package ch.epfl.sweng.tutosaurus;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.test.espresso.contrib.NavigationViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.PreferenceMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerActions.close;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
-/**
- * Created by Vincent on 14/10/2016.
- */
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-
 
     @Rule
     public IntentsTestRule<MainActivity> mainActivityRule = new IntentsTestRule<MainActivity>(
@@ -29,21 +31,33 @@ public class MainActivityTest {
     );
 
     @Test
-    public void testLogIn() {
+    public void testSettingTab() throws InterruptedException{
+        onView(withId(R.id.main_email)).perform(typeText("vincent.rinaldi@epfl.ch"));
+        closeSoftKeyboard();
+        onView(withId(R.id.main_password)).perform(typeText("mrstvm95"));
+        closeSoftKeyboard();
         onView(withId(R.id.connectionButton)).perform(click());
+        Thread.sleep(1000);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext());
         onView(withId(R.id.drawer_layout)).perform(open());
-        onView(withId(R.id.drawer_layout)).perform(close());
-        onView(withId(R.id.action_logOutButton)).perform(click());
-    }
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_settings_layout));
 
-    @Test
-    public void testSignUp() {
-        onView(withId(R.id.registerButton)).perform(click());
-        onView(withId(R.id.confirmButton)).perform(click());
-        onView(withId(R.id.firstNameProvided)).check(matches(withText("First name : Vincent")));
-        onView(withId(R.id.lastNameProvided)).check(matches(withText("Last name : Rinaldi")));
-        onView(withId(R.id.emailAddressProvided)).check(matches(withText("Email address : vincent.rinaldi@epfl.ch")));
-        onView(withId(R.id.sciperProvided)).check(matches(withText("Sciper : 239759")));
-        onView(withId(R.id.backToLoginButton)).perform(click());
+        if (sharedPreferences.getBoolean("checkbox_preference_notification", true)) {
+            onData(PreferenceMatchers.withKey("checkbox_preference_notification")).perform(click());
+            assertThat(sharedPreferences.getBoolean("checkbox_preference_notification", true), equalTo(false));
+        } else {
+            onData(PreferenceMatchers.withKey("checkbox_preference_notification")).perform(click());
+            assertThat(sharedPreferences.getBoolean("checkbox_preference_notification", true), equalTo(true));
+        }
+
+        if (sharedPreferences.getBoolean("checkbox_preference_calendar", true)) {
+            onData(PreferenceMatchers.withKey("checkbox_preference_calendar")).perform(click());
+            assertThat(sharedPreferences.getBoolean("checkbox_preference_calendar", true), equalTo(false));
+        } else {
+            onData(PreferenceMatchers.withKey("checkbox_preference_calendar")).perform(click());
+            assertThat(sharedPreferences.getBoolean("checkbox_preference_calendar", true), equalTo(true));
+        }
+
+        onView(withId(R.id.action_logOutButton)).perform(click());
     }
 }
