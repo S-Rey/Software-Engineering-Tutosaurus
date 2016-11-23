81b2c92b7f5deffd150a531d7310aef07385cbda
Alberto Chiappa <alberto.chiappa@epfl.ch>
2016-11-23 03:02:05 +0100

    Now the course list to search for tutors gets the courses from FullCourseList singleton and uses ClassicCourseAdapter to be populated

--- app/src/main/java/ch/epfl/sweng/tutosaurus/model/FullCourseList.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/model/FullCourseList.java
@@ -1,7 +1,6 @@
 package ch.epfl.sweng.tutosaurus.model;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import ch.epfl.sweng.tutosaurus.R;
 
@@ -10,19 +9,21 @@ import ch.epfl.sweng.tutosaurus.R;
  */
 public class FullCourseList {
     private static FullCourseList ourInstance = new FullCourseList();
-    private static ArrayList<Course> listOfCourses;
+    private ArrayList<Course> listOfCourses;
 
     public static FullCourseList getInstance() {
         return ourInstance;
     }
 
     private FullCourseList() {
-        listOfCourses.add(new Course("Maths", "Mathematics", R.drawable.school));
-        listOfCourses.add(new Course("Physics", "Physics", R.drawable.molecule));
-        listOfCourses.add(new Course("Chemistry", "Chemistry", R.drawable.flask));
-        listOfCourses.add(new Course("Computer", "Computer Science", R.drawable.computer));
-        listOfCourses.add(new Course("Quantum", "Quantum Mechanics", R.drawable.music));
-        listOfCourses.add(new Course("Dynamics", "Dynamics", R.drawable.shapes));
+        ArrayList<Course> listOfCourses = new ArrayList<>(0);
+        listOfCourses.add(new Course("mathematics", "Mathematics", R.drawable.school));
+        listOfCourses.add(new Course("physics", "Physics", R.drawable.molecule));
+        listOfCourses.add(new Course("chemistry", "Chemistry", R.drawable.flask));
+        listOfCourses.add(new Course("computer_science", "Computer Science", R.drawable.computer));
+        listOfCourses.add(new Course("quantum_mechanics", "Quantum Mechanics", R.drawable.music));
+        listOfCourses.add(new Course("dynamics", "Dynamics", R.drawable.shapes));
+        this.listOfCourses = listOfCourses;
     }
 
     public ArrayList<Course> getListOfCourses(){
