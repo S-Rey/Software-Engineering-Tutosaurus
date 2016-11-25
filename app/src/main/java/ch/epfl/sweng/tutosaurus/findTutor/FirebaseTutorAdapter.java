39ce7314e3860b0d52c35940e8f8644be630574a
ubervison <basile.thullen@gmail.com>
2016-11-25 18:18:40 +0100

    get picture from storage in TutorAdapter

--- app/src/main/java/ch/epfl/sweng/tutosaurus/findTutor/FirebaseTutorAdapter.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/findTutor/FirebaseTutorAdapter.java
@@ -2,12 +2,20 @@ package ch.epfl.sweng.tutosaurus.findTutor;
 
         import android.app.Activity;
         import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.support.annotation.NonNull;
+        import android.util.Log;
         import android.view.View;
         import android.widget.ImageView;
         import android.widget.TextView;
 
         import com.firebase.ui.database.FirebaseListAdapter;
+        import com.google.android.gms.tasks.OnFailureListener;
+        import com.google.android.gms.tasks.OnSuccessListener;
         import com.google.firebase.database.Query;
+        import com.google.firebase.storage.FirebaseStorage;
+        import com.google.firebase.storage.StorageReference;
 
         import ch.epfl.sweng.tutosaurus.PublicProfileActivity;
         import ch.epfl.sweng.tutosaurus.R;
@@ -19,6 +27,8 @@ package ch.epfl.sweng.tutosaurus.findTutor;
 
 public class FirebaseTutorAdapter extends FirebaseListAdapter<User> {
 
+    public static final String TAG = "FirebaseTutorAdapter";
+
     public FirebaseTutorAdapter(Activity activity, java.lang.Class<User> modelClass, int modelLayout, Query ref) {
         super(activity, modelClass, modelLayout, ref);
     }
@@ -27,7 +37,7 @@ public class FirebaseTutorAdapter extends FirebaseListAdapter<User> {
     protected void populateView(View view, User tutor, int position) {
         TextView profileName = (TextView) view.findViewById(R.id.profileName);
         profileName.setText(tutor.getFullName());
-        ImageView profilePicture = (ImageView) view.findViewById(R.id.profilePicture);
+        final ImageView profilePicture = (ImageView) view.findViewById(R.id.profilePicture);
         profilePicture.setImageResource(tutor.getPicture());
 
         // Set the OnClickListener on each name of the list
@@ -38,6 +48,23 @@ public class FirebaseTutorAdapter extends FirebaseListAdapter<User> {
             @Override
             public void onClick(View v) {
                 v.getContext().startActivity(intent);
+            }
+        });
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://tutosaurus-16fce.appspot.com");
+        Log.d(TAG, "tutor: " +tutor.getSciper() + " " + tutor.getFullName());
+        StorageReference picRef = storageRef.child("profilePictures").child(tutor.getSciper()+".png");
+        picRef.getBytes(1024*1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap pic = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                profilePicture.setImageBitmap(pic);
+                Log.d(TAG, "success");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "failure");
             }
         });
     }
