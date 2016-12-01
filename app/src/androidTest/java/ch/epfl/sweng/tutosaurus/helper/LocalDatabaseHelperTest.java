3b61708c0593e91cbd8d5e39092d2c5935f28cc1
SamGod <samuel.rey@epfl.ch>
2016-12-01 14:30:06 +0100

    Basic test: (setup, tearDown, getDatabase)

--- app/src/androidTest/java/ch/epfl/sweng/tutosaurus/helper/LocalDatabaseHelperTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/tutosaurus/helper/LocalDatabaseHelperTest.java
@@ -1,8 +1,45 @@
 package ch.epfl.sweng.tutosaurus.helper;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.LocaleList;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
 /**
  * Created by samuel on 29.11.16.
  */
 
+@RunWith(AndroidJUnit4.class)
 public class LocalDatabaseHelperTest {
+
+    private SQLiteOpenHelper dbHelper;
+    private SQLiteDatabase database;
+
+    @Before
+    public void setup() {
+        getTargetContext().deleteDatabase(LocalDatabaseHelper.TABLE_COURSE);
+        getTargetContext().deleteDatabase(LocalDatabaseHelper.TABLE_USER);
+        getTargetContext().deleteDatabase(LocalDatabaseHelper.TABLE_LANGUAGE);
+        dbHelper = new LocalDatabaseHelper(getTargetContext());
+        database = dbHelper.getWritableDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        LocalDatabaseHelper.clear(database);
+    }
+
+    @Test
+    public void testGetDatabase() {
+        dbHelper.getReadableDatabase();
+        dbHelper.getWritableDatabase();
+    }
+
 }
