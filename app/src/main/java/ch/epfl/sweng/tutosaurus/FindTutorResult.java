24ee791d77894ccb461efa3a1791767e43c01672
ubervison <basile.thullen@gmail.com>
2016-12-09 12:51:27 +0100

    cleanup

--- app/src/main/java/ch/epfl/sweng/tutosaurus/FindTutorResult.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/FindTutorResult.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+
 import com.google.firebase.database.Query;
 
 import ch.epfl.sweng.tutosaurus.adapter.FirebaseTutorAdapter;
--- app/src/main/java/ch/epfl/sweng/tutosaurus/adapter/FirebaseTutorAdapter.java
+++ app/src/main/java/ch/epfl/sweng/tutosaurus/adapter/FirebaseTutorAdapter.java
@@ -1,37 +1,26 @@
 package ch.epfl.sweng.tutosaurus.adapter;
 
 import android.app.Activity;
-        import android.content.Intent;
-        import android.graphics.Bitmap;
-        import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.support.annotation.NonNull;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
-import com.google.android.gms.tasks.OnFailureListener;
-        import com.google.android.gms.tasks.OnSuccessListener;
-        import com.google.firebase.database.Query;
-        import com.google.firebase.storage.FirebaseStorage;
-        import com.google.firebase.storage.StorageReference;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import ch.epfl.sweng.tutosaurus.PublicProfileActivity;
-        import ch.epfl.sweng.tutosaurus.R;
-        import ch.epfl.sweng.tutosaurus.model.User;
+import ch.epfl.sweng.tutosaurus.R;
+import ch.epfl.sweng.tutosaurus.model.User;
 
 public class FirebaseTutorAdapter extends FirebaseListAdapter<User> {
 
     private static final String TAG = "FirebaseTutorAdapter";
-
-    private Map<Integer, ImageView> picMap = new HashMap<>();
 
     Activity activity;
 
@@ -46,9 +35,6 @@ public class FirebaseTutorAdapter extends FirebaseListAdapter<User> {
         TextView profileName = (TextView) view.findViewById(R.id.profileName);
         profileName.setText(tutor.getFullName());
         ImageView profilePicture = (ImageView) view.findViewById(R.id.profilePicture);
-        profilePicture.setImageResource(R.drawable.calculator);
-        picMap.put(position, profilePicture);
-        //profilePicture.setImageResource(tutor.getPicture());
 
         // Set the OnClickListener on each name of the list
         final Intent intent = new Intent(view.getContext(), PublicProfileActivity.class);
