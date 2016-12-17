09ae94092a5331cacdf5bc64528ae4e5193b63c1
Santo Gioia <santo.gioia@epfl.ch>
2016-12-17 19:21:37 +0100

    fix test

--- app/src/androidTest/java/ch/epfl/sweng/tutosaurus/MeetingTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/tutosaurus/MeetingTest.java
@@ -104,9 +104,6 @@ public class MeetingTest{
         Thread.sleep(2000);
         onData(anything()).inAdapterView(withId(R.id.meetingList)).atPosition(0).
                 onChildView(withId(R.id.showDetailsMeeting)).perform(click());
-        onData(anything()).inAdapterView(withId(R.id.meetingList)).atPosition(0).
-                onChildView(withId(R.id.syncCalendar)).perform(click());
-
     }
 
     
